13e5997d3b3e9395bfc546786367c4237ffa74ce
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-04-28 22:39:45 -0700

    image upload

--- SampleApi/Sample.Api.Customer/Controllers/FileController.cs
+++ SampleApi/Sample.Api.Customer/Controllers/FileController.cs
@@ -80,11 +80,13 @@ namespace Sample.Api.Customer.Controllers
                 //var filePath = Path.GetTempFileName();
 
                 string connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1";
-                var containterName = "image";
+                var containterName = "customer-profile-picture";
                 var fileName = "file";
                 BlobContainerClient container = new BlobContainerClient(connectionString, containterName);
 
                 BlobClient blob = container.GetBlobClient(fileName);
+
+
 
                 BlobHttpHeaders blobHttpHeaders = new BlobHttpHeaders();
                 blobHttpHeaders.ContentType = "image/jpeg";
@@ -92,7 +94,9 @@ namespace Sample.Api.Customer.Controllers
 
                 using (var stream = formFile.OpenReadStream())
                 {
+                    var rr2 = blob.Uri.AbsoluteUri;
                     await blob.UploadAsync(stream, blobHttpHeaders);
+                    var rr = blob.Uri.AbsoluteUri;
                 }
             }
 
@@ -151,7 +155,7 @@ namespace Sample.Api.Customer.Controllers
                 }
             }
 
-         
+
         }
 
 
