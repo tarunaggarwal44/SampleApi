c35dab26698dec77b108f520ff98e404e2ad7256
tarun aggarwal <tarun.aggarwal4@gmail.com>
2022-03-07 14:08:38 +0530

    Docker

---
+++ SampleApi/Sample.Api.Customer/Controllers/PatientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Api.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Customer.Controllers
+{
+    //[Route("api/v1/customers")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : BaseApiController
+    {
+
+        //GET: api/<CustomerController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await Task.FromResult(this.Ok("Hello"));
+        }
+
+
+      
+    }
+}
